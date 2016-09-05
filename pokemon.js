3a303eae25444239bf5de5bebde71a47a4d1cf02
estevetarra <ets1995@hotmail.com>
2016-09-05 11:28:24 +0200

    Update pokemon.js

--- pokemon.js
+++ pokemon.js
@@ -9,4 +9,21 @@ function pokemon(spiece, level, charge_move, quick_move , ivatt, ivdeff, ivsta)
     this.ivsta = ivsta;
 }
 
+function fighter(pokemon, baseatt, basedeff, basehp, deffenfing)
+{
+    this.pokemon = pokemon;
+    this.baseatt = baseatt;
+    this.basedeff = basedeff;
+    this.basehp = basehp
+    this.deffenfing = deffenfing;
+    this.cooldown = 0;
+    
+}
+
+function battle(fighter1, fighter2)
+{
+    this.fighter1 = fighter1;
+    this.fighter2 = fighter2;
+    this.time = 0;
+}
 
