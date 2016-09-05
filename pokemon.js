3c974060f288e80fc6cb0a6dc2b08b1e9d6962df
estevetarra <ets1995@hotmail.com>
2016-09-05 11:08:37 +0200

    pokemon creator

---
+++ pokemon.js
@@ -0,0 +1,12 @@
+function pokemon(spiece, level, charge_move, quick_move , ivatt, ivdeff, ivsta) 
+{
+    this.spiece = spiece;
+    this.level = level;
+    this.charge_move = charge_move;
+    this.quick_move = quick_move;
+    this.ivatt = ivatt;
+    this.ivdeff = ivdeff;
+    this.ivsta = ivsta;
+}
+
+
