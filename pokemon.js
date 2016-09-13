4ce78dfaf56fb50e6293d158a842419980db43dc
estevetarra <ets1995@hotmail.com>
2016-09-13 19:00:02 +0200

    Let's go!

--- pokemon.js
+++ pokemon.js
@@ -23,7 +23,9 @@ function Fighter(pokemon, baseatt, basedeff, basehp, deffending)
     this.pokemon = pokemon;
     this.baseatt = baseatt;
     this.basedeff = basedeff;
-    this.basehp = basehp
+    this.basehp = basehp;
+    this.att = (baseatt + this.pokemon.ivatt) * this.pokemon.levelmultiplayer;
+    this.deff = (basedeff + this.pokemon.ivdeff) * this.pokemon.levelmultiplayer;
     this.deffending = deffending;
     this.cooldown = -10;
     this.nowmove = "NONE";
@@ -51,32 +53,64 @@ function Battle(fighter1, fighter2, quick_moves, charge_moves)
         {
             //inflict damage to the enemy
             //Update energy of both
+            var stab = 1;
+            var effectiveness = 1;
+            var power = 1;
+            var damage = Math.floor(0.5 * power * (fighter1.att / figthter2.deff) * stab * effectiveness);
+            
+            fighter2.hp -= damage;
+            //Notice that this isn't correct... fighter2 have advantadge
+            fighter2.energy += damage/2;
+            fighter1.energy += 2;
         }
         if(fighter1.cooldown <= 0)
         {
           //decide next move
-          //Update energy
+          //Update energy+
+          fighter1.cooldown = 10;
+
+        }
+        if(fighter1.cooldown == 0)
+        {
+            //inflict damage to the enemy
+            //Update energy of both
+            var stab = 1;
+            var effectiveness = 1;
+            var power = 1;
+            var damage = Math.floor(0.5 * power * (fighter2.att / figthter1.deff) * stab * effectiveness);
+            
+            fighter1.hp -= damage;
+            //Notice that this isn't correct... fighter2 have advantadge
+            fighter1.energy += damage/2;
+            fighter2.energy += 2;
+        }
+        if(fighter2.cooldown <= 0)
+        {
+          //decide next move
+          //Update energy+
+          fighter2.cooldown = 10;
 
         }
         
+        fighter1.cooldown -= 10;
+        fighter2.cooldown -= 10;
         //decrease cooldowmn
         
     };
     this.getWinner = function getWinner()
     {
-        return this.fighter1.pokemon.spiece;
-        //If both of them are alive
-        if(fighter1.cooldown == 0)
-        {
-            //inflict damage to the enemy
-            //Update energy of both
-        }
-        if(fighter1.cooldown <= 0)
-        {
-          //decide next move
-          //Update energy
-
-        }
+    	while(this.fighter1.hp > 0 && this.fighter2.hp > 0)
+    	{
+    		this.next_turn();
+    	}
+    	if(this.fighter1.hp <= 0 )
+    	{
+    		return this.fighter1.pokemon.spiece;
+    	} 
+    	else
+    	{
+    		return this.fighter2.pokemon.spiece;
+    	}
         
         //decrease cooldowmn
         
